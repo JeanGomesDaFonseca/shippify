6e9087194c81d2bcfacb9e19c64e20f37a1068c4
JeanGomesDaFonseca <jeangomes.dev@outlook.com>
2025-02-12 22:14:54 -0300

    iniciando testes

--- jest.config.mjs
+++ jest.config.mjs
@@ -4,7 +4,8 @@ export default {
   },
   moduleNameMapper: {
     "\\.(css|less|scss|sass)$": "identity-obj-proxy",
-    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2)$": "<rootDir>/__mocks__/fileMock.js"
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2)$": "<rootDir>/src/__mocks__/fileMock.js"
   },
   setupFilesAfterEnv: ["<rootDir>/src/setupTests.js"],
+  testEnvironment: "jest-environment-jsdom",
 };
